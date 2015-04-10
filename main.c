d76a57ace93e9f1e96aaa258996deb029944961b
Akash Raj <akashrajkn@gmail.com>
2015-04-10 16:10:22 +0530

    added main program

---
+++ main.c
@@ -0,0 +1,49 @@
+/*
+  Copyright (c) data-fetish
+
+  Team: Knowledge-mining
+
+  Tuthors:  Akash Raj K N
+            Gopichand Paturi
+            Anjali Thakur
+*/
+
+/*
+    FP-Growth Algorithm
+    main.c
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+  /*
+     - get the min support count or percentage from user
+       
+     - FP-Tree-Generation
+       - generate frequent itemset 
+       - sort the input transactions in descending order based on frequent itemset support count
+       - create the FP-tree
+
+     - FP-Growth
+
+  */
+
+  long long minSupportCount = 0;
+
+  printf( "Enter the minimum support count: " );
+  scanf( "%lld", &minSupportCount );
+
+  //FP_Tree_Generation( minSupportCount );
+
+  //FP_Growth();
+
+  return 0;
+}
+
+
+
