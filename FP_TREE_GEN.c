9613e5695ed6e0a7210f8e9d8a53ae8c733c3ed4
Akash Raj <akashrajkn@gmail.com>
2015-04-11 01:06:40 +0530

    added FP_TREE_GEN files

---
+++ FP_TREE_GEN.c
@@ -0,0 +1,31 @@
+/*
+  Copyright (c) data-fetish
+
+  Team: Knowledge-mining
+
+  Authors:  Akash Raj K N
+            Gopichand Paturi
+            Anjali Thakur
+*/
+
+/*
+    FP-Growth Algorithm
+    FP_TREE_GEN.c
+*/
+
+#include "FP_TREE_GEN.h"
+
+
+//global variables
+long long minSupportCount = 0;
+
+
+
+FP_Tree_Generation( long long minSupport )
+{
+  minSupportCount = minSupport;
+
+  generateFrequentItemSets();  //from the input file, generate the frequent itemsets
+}
+
+
---
+++ FP_TREE_GEN.h
@@ -0,0 +1,31 @@
+/*
+  Copyright (c) data-fetish
+
+  Team: Knowledge-mining
+
+  Authors:  Akash Raj K N
+            Gopichand Paturi
+            Anjali Thakur
+*/
+
+/*
+    FP-Growth Algorithm
+    FP_TREE_GEN.h
+*/
+
+
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+FP_Tree_Generation(long long);
+
+generateFrequentItemSets();
+
+sortInputTransaction();
+
+createFPTree();
