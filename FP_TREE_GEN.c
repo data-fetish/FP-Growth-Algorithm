07843d62079d05b4e4a894a81b9ebbe5d70c1ad2
akashraj <akashrajkn@gmail.com>
2015-04-23 02:50:55 +0530

    fp tree constructed

--- FP_TREE_GEN.c
+++ FP_TREE_GEN.c
@@ -28,8 +28,10 @@ long long numFreqItems;
 struct itemNode *itemSet;
 long long *frequentItems;
 struct treeNode *head = NULL;
-struct fList *freqList;
+//struct fList *freqList;
 //struct fList tempFList;
+
+struct fList freqList[100]; /* have to make it dynamic allocation */
 
 FILE *fp;
 FILE *freqp;
@@ -94,13 +96,13 @@ void generateFrequentItemSets()
 
   /* create the table for the FP-Tree */
   numFreqItems = i; //number of frequent items (with support > than minSupportCount)
-
-  freqList = (struct fList *)malloc(100*sizeof(struct fList)); // frequent itemList table
+printf("numFreqItems= === %lld\n", numFreqItems);
+  //freqList = (struct fList *)malloc(100*sizeof(struct fList)); // frequent itemList table
 
   for( i=0; i<numFreqItems; ++i )
-  {
+  {printf("freqlist %lld\n",itemSet[i].id );
     freqList[i].id = itemSet[i].id;
-    freqList[i].support = itemSet[i].id;
+    freqList[i].support = itemSet[i].support;
     freqList[i].ptr = NULL;
   }
 
@@ -208,7 +210,156 @@ int comparator2( const void *p, const void *q )
 
 void createFPTree()
 {
+  freqp = fopen("frequent.txt", "r");
+
+  char buf[500];
+  long long currentNum, i, j, l;
+  struct treeNode *currentPtr; //points to the current transaction path in the tree
+  struct treeNode *horizontalPtr, *prevHorizontal, **tempPtr1;
+
+  // initialize the null node 
+  head = (struct treeNode *)malloc(sizeof(struct treeNode));
+  head->children = (struct treeNode **)malloc((numFreqItems+1) * sizeof(struct treeNode *));
+  for(j=0; j<=numFreqItems; ++j)
+  {
+    head->children[j] = NULL;
+  }
+  head->parent = NULL;
+  head->id = 0;
+  head->count = -1;
+  head->horizontal = NULL;
+
+
+  while( fgets(buf, 500, freqp)!=NULL ) //start reading the transactions line by line
+  {
+    currentNum = 0;
+    currentPtr = head;
+
+    for( i = 0; i<strlen(buf); ++i )
+    {
+      if(buf[i]!=' ')
+      {
+        currentNum *= 10;
+        currentNum += (buf[i]-'0');
+      }
+      else
+      {
+        /* add the current num into the tree */
+
+        //find the currentNum index in freqList array --- can be improved with binary search
+        //linear search
+        for(j=0; j<numFreqItems; ++j)
+        {
+          if(freqList[j].id == currentNum)
+          {
+            break;
+          }
+        }
+        //printf("currentNum %lld   ,", freqList[j].id);
+        //if(currentNum==75)
+          //printf("yay %lld\n", j);
+        if(currentPtr->children[j] == NULL)
+        {
+          //printf("children[%lld] is null\n", j);
+          currentPtr->children[j] = (struct treeNode *)malloc(sizeof(struct treeNode));
+          currentPtr->children[j]->id = currentNum;
+          currentPtr->children[j]->parent = currentPtr;
+          currentPtr->children[j]->count = 1;
+          currentPtr->children[j]->horizontal = NULL; //if you remove this stmt, all hell will break loose
+
+          struct treeNode **temp2ptr;
+
+          if(freqList[j].ptr == NULL)
+          {
+            freqList[j].ptr = currentPtr->children[j]; 
+          }
+          else //if(currentPtr->children[j]->horizontal == NULL)
+          {
+            horizontalPtr = freqList[j].ptr;
+            prevHorizontal = NULL;  
+
+            while(horizontalPtr != NULL)
+            {
+              prevHorizontal = horizontalPtr;
+              temp2ptr = &(horizontalPtr->horizontal);
+              horizontalPtr = *temp2ptr;
+            }
+
+            if(prevHorizontal != currentPtr->children[j])
+            {
+              *temp2ptr = currentPtr->children[j];  
+            }
+            
+            //print the path here
+          }
+//////
+/*          prevHorizontal = freqList[j].ptr;
+          while(prevHorizontal!=NULL)
+          {
+            printf("%lld->", prevHorizontal->id);
+            prevHorizontal = prevHorizontal->horizontal;
+          }
+          printf("\n\n");
+*/         
+//////          
+          currentPtr->children[j]->children = (struct treeNode **)malloc((numFreqItems+1) * sizeof(struct treeNode));
+          for(l=0; l<=numFreqItems; ++l)
+          {
+            currentPtr->children[j]->children[l] = NULL;
+          }
+
+        }
+        else
+        {//printf("what\n");
+          currentPtr->children[j]->count++;
+
+          //currentPtr = currentPtr->children[j];
+        }
+        
+        currentPtr = currentPtr->children[j]; // point to next child in the path
+
+        currentNum = 0;
+      }
+    }
+
+  }
+
+
+//iterate all the freqList items
+
+  struct treeNode *tempPtr;
+  for(i=0; i<=numFreqItems; ++i)
+  {
+    printf("%lld\n", i);
+    if(freqList[i].ptr == NULL)
+    {
+      //printf("325 \n");
+      printf("[%lld, null]\n", freqList[i].id);
+    }
+    else
+    {
+      //printf("330 \n");
+      printf("[%lld, %lld] ->",freqList[i].id, freqList[i].support );
+      currentPtr = freqList[i].ptr;
+
+      while(currentPtr!=NULL)
+      {
+        printf("(%lld, %lld)->", currentPtr->id, currentPtr->count );
+
+        currentPtr = currentPtr->horizontal;
+      }
+
+      printf("\n\n");
+    }
+    //break;
+  }
+
+
+  ////print the tree
 
 
 
+
+
+  fclose(freqp);
 }
